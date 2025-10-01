2fae96695e069ba7b694575eed9a021a68baf210
Huguito <110265653+BondiolaAlHorno@users.noreply.github.com>
2025-09-30 23:04:46 -0300

    Update db.py and PrestatarioService.py

--- app/db.py
+++ app/db.py
@@ -7,6 +7,15 @@ engine = create_engine(DATABASE_URL, connect_args={"check_same_thread":False})
 
 Base = declarative_base()
 
-Session = sessionmaker(bind=engine)
+# Session = sessionmaker(bind=engine)
 
-session = Session()
+# session = Session()
+
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+def get_session():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
--- app/service/PrestatarioService.py
+++ app/service/PrestatarioService.py
@@ -1,28 +1,27 @@
 from model.Prestatario import Prestatario
-from db import session
+from sqlalchemy.orm import Session
+from fastapi import Depends
+from db import get_session
 from framework import app
 
 @app.post("/create_prestatario/",tags=["Prestatario"],)
-def create(nombre:str="",
-                   apellido:str="",
-                   edad:int=0,
-                   dni:int=1234567):
+def create(nombre:str="",apellido:str="",edad:int=0,dni:int=1234567,session: Session = Depends(get_session)):
     
     prestatario = Prestatario(nombre=nombre,
-                      apellido=apellido,
-                      edad=edad,
-                      dni=dni)
+                              apellido=apellido,
+                              edad=edad,
+                              dni=dni)
     
     session.add(prestatario)
     session.commit()
     session.refresh(prestatario)
 
 @app.get("/get_all_prestatarios/",tags=["Prestatario"])
-def get_all():
+def get_all(session: Session = Depends(get_session)):
     return session.query(Prestatario).all()
 
 @app.get("/get_prestatario_by_id/{item_id}",tags=["Prestatario"])
-def get_by_id(item_id:int):
+def get_by_id(item_id:int,session: Session = Depends(get_session)):
     response = session.get(Prestatario,item_id)
     if response == None:
         return {"Prestatario no encontrada"}
@@ -31,7 +30,7 @@ def get_by_id(item_id:int):
 
 
 @app.post("/delete_prestatario_by_id/{item_id}",tags=["Prestatario"])
-def delete_by_id(item_id:int):
+def delete_by_id(item_id:int,session: Session = Depends(get_session)):
     response = session.get(Prestatario,item_id)
     if response == None:
         return {"Prestatario no encontrada"}
